30d6836780cf0deff8119c1d6e6846f822838311
andres vallecilla <andresfelipe083195@hotmail.com>
2026-06-25 12:24:30 -0500

    refactor: linter
    Signed-off-by: andres vallecilla <andresfelipe083195@hotmail.com>

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -295,14 +295,6 @@ export const SERVICE = {
       key: "ExchangeRateProcessorService",
       path: "v1.ExchangeRateProcessorService",
     },
-    ExchangeRateDatabaseService: {
-      key: "ExchangeRateDatabaseService",
-      path: "v1.ExchangeRateDatabaseService",
-    },
-    ExchangeRateProcessorService: {
-      key: "ExchangeRateProcessorService",
-      path: "v1.ExchangeRateProcessorService",
-    },
     CrawlAccountService: {
       key: 'CrawlAccountService',
       name: 'v1.CrawlAccountService',
