047fdd5f5432b9d0768581f6185cd2f342e63fe0
andresfv95 <andresfelipe083195@hotmail.com>
2026-07-01 11:02:26 -0500

    feat: implemented IDX-XR-QRY-1 (#299)
    Signed-off-by: andres vallecilla <andresfelipe083195@hotmail.com>

--- src/services/api/api.service.ts
+++ src/services/api/api.service.ts
@@ -354,6 +354,7 @@ function createRoute(path: string, aliases: Record<string, string>, requireBlock
       }),
       createRoute('/v4/exchange-rate', {
         'GET price': `${SERVICE.V1.ExchangeRateApiService.path}.getPrice`,
+        'GET get': `${SERVICE.V1.ExchangeRateApiService.path}.getExchangeRate`,
       }),
       createRoute('/verana/td/v1', {
         'GET get/:corporation': `${SERVICE.V1.TrustDepositApiService.path}.getTrustDeposit`,
