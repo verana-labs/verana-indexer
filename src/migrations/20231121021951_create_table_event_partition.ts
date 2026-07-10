8b7ec9de19d24b642d5926bcd88b54c05c5635dc
andres vallecilla <andresfelipe083195@hotmail.com>
2026-07-10 08:11:02 -0500

    fix: solve problem with events
    Signed-off-by: andres vallecilla <andresfelipe083195@hotmail.com>

--- src/migrations/20231121021951_create_table_event_partition.ts
+++ src/migrations/20231121021951_create_table_event_partition.ts
@@ -1,4 +1,5 @@
 import { Knex } from 'knex'
+import config from '../config.json' with { type: 'json' }
 
 export async function up(knex: Knex): Promise<void> {
   await knex.transaction(async (trx) => {
@@ -24,9 +25,28 @@ export async function up(knex: Knex): Promise<void> {
         ON event_partition USING BTREE (block_height ASC NULLS LAST);`
     )
 
+    const startId = config.migrationEventToPartition.startId
+    const endId = config.migrationEventToPartition.endId
+    const step = config.migrationEventToPartition.step
+    for (let i = startId; i < endId; i += step) {
+      const partitionName = `event_partition_${i}_${i + step}`
+      await knex.raw(`CREATE TABLE IF NOT EXISTS ${partitionName} (LIKE event_partition INCLUDING ALL)`).transacting(trx)
+      await knex
+        .raw(`ALTER TABLE event_partition ATTACH PARTITION ${partitionName} FOR VALUES FROM (${i}) TO (${i + step})`)
+        .transacting(trx)
+    }
+
     // Change table name if no data exist on event
     const isExistEventData = await knex.raw(`SELECT * FROM event LIMIT 1`)
     if (isExistEventData.rows.length === 0 && process.env.NODE_ENV !== 'test') {
+      await knex
+        .raw(
+          `
+          ALTER TABLE event_attribute DROP CONSTRAINT IF EXISTS event_attribute_partition_event_id_foreign CASCADE;
+          ALTER TABLE smart_contract_event DROP CONSTRAINT IF EXISTS smart_contract_event_event_id_foreign CASCADE;
+        `
+        )
+        .transacting(trx)
       await knex.raw('ALTER TABLE event RENAME TO event_backup;').transacting(trx)
       await knex.raw('ALTER TABLE event_partition RENAME TO event;').transacting(trx)
     }
