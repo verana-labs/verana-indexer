f8cc7e2bf46c9be482d438bad3c7f368bc8eb4cf
pratikasr <pratikasr@gmail.com>
2026-08-27 19:29:45 +0530

    fix(co): activate GF versions from chain events to prevent EGF wedge

--- src/common/utils/gf_events.ts
+++ src/common/utils/gf_events.ts
@@ -33,6 +33,29 @@ export function extractAddGfDocumentEvents(txEvents: TxEvent[] | undefined): Add
   return out
 }
 
+export interface IncreaseGfActiveEvent {
+  gfvId: number | null
+  version: number | null
+}
+
+export function extractIncreaseGfActiveEvent(
+  txEvents: TxEvent[] | undefined,
+  ecosystemId: number
+): IncreaseGfActiveEvent | undefined {
+  if (!Array.isArray(txEvents)) return undefined
+  for (const ev of txEvents) {
+    if ((ev.type ?? '') !== 'increase_active_gf_version') continue
+    const attrs = new Map<string, string>()
+    for (const a of ev.attributes ?? []) attrs.set(a.key ?? '', (a.value ?? '').replace(/^"|"$/g, ''))
+    if (Number(attrs.get('ecosystem_id')) !== ecosystemId) continue
+    return {
+      gfvId: toId(attrs.get('gfv_id')),
+      version: toId(attrs.get('version')),
+    }
+  }
+  return undefined
+}
+
 export function matchAddGfDocumentEvent(
   events: AddGfDocumentEvent[],
   version: number,
